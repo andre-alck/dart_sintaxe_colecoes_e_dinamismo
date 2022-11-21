abcc92fe8f986ecf67c0f5a9c2c6440978664986
andre-alck <andrealck1@gmail.com>
2022-11-21 12:07:07 -0300

    first commit

---
+++ bin/dart_sintaxe_colecoes_e_dinamismo.dart
@@ -0,0 +1,5 @@
+import 'package:dart_sintaxe_colecoes_e_dinamismo/dart_sintaxe_colecoes_e_dinamismo.dart' as dart_sintaxe_colecoes_e_dinamismo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_sintaxe_colecoes_e_dinamismo.calculate()}!');
+}
---
+++ test/dart_sintaxe_colecoes_e_dinamismo_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_sintaxe_colecoes_e_dinamismo/dart_sintaxe_colecoes_e_dinamismo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
