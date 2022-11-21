666a04487c7c9fa16c116e225cabeb2a4bd7ca38
andre-alck <andrealck1@gmail.com>
2022-11-21 12:52:46 -0300

    feat: add 01

--- bin/dart_sintaxe_colecoes_e_dinamismo.dart
+++ bin/dart_sintaxe_colecoes_e_dinamismo.dart
@@ -1,5 +1,45 @@
-import 'package:dart_sintaxe_colecoes_e_dinamismo/dart_sintaxe_colecoes_e_dinamismo.dart' as dart_sintaxe_colecoes_e_dinamismo;
+void main() {
+  // escolherMeioDeTransporteInt(0);
+  escolherMeioDeTransporteEnum(Transporte.andando);
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_sintaxe_colecoes_e_dinamismo.calculate()}!');
+// void escolherMeioDeTransporteInt(int locomocao) {
+//   if (locomocao == 0) {
+//     print("Carro.");
+//   } else if (locomocao == 1) {
+//     print("Bike.");
+//   } else {
+//     print("Meio de transporte não identificado.");
+//   }
+// }
+
+enum Transporte { andando, aviao, bike, carro, patins, skate, trem }
+
+void escolherMeioDeTransporteEnum(Transporte locomocao) {
+  switch (locomocao) {
+    // case Transporte.andando:
+    //   print("Andando.");
+    //   break;
+    case Transporte.aviao:
+      print("Avião.");
+      break;
+    case Transporte.bike:
+      print("Bike.");
+      break;
+    case Transporte.carro:
+      print("Carro.");
+      break;
+    case Transporte.patins:
+      print("Patins.");
+      break;
+    case Transporte.skate:
+      print("Skate.");
+      break;
+    case Transporte.trem:
+      print("Trem.");
+      break;
+    default:
+      print("Meio de transporte não identificado.");
+      break;
+  }
 }
--- test/dart_sintaxe_colecoes_e_dinamismo_test.dart
+++ test/dart_sintaxe_colecoes_e_dinamismo_test.dart
@@ -1,8 +1,15 @@
-import 'package:dart_sintaxe_colecoes_e_dinamismo/dart_sintaxe_colecoes_e_dinamismo.dart';
 import 'package:test/test.dart';
 
+import '../bin/dart_sintaxe_colecoes_e_dinamismo.dart';
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('numerations', () {
+    expect(Transporte.andando.index, 0);
+    expect(Transporte.aviao.index, 1);
+    expect(Transporte.bike.index, 2);
+    expect(Transporte.carro.index, 3);
+    expect(Transporte.patins.index, 4);
+    expect(Transporte.skate.index, 5);
+    expect(Transporte.trem.index, 6);
   });
 }
