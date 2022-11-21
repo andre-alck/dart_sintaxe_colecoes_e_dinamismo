ff5149bd46e0150d7b5d5018acbaf5592fcb09d9
andre-alck <andrealck1@gmail.com>
2022-11-21 13:47:53 -0300

    feat: add 1/2 02

--- bin/dart_sintaxe_colecoes_e_dinamismo.dart
+++ bin/dart_sintaxe_colecoes_e_dinamismo.dart
@@ -1,45 +1,54 @@
 void main() {
-  // escolherMeioDeTransporteInt(0);
-  escolherMeioDeTransporteEnum(Transporte.andando);
-}
+  escolherMeioDeTransporte(Transporte.andando);
 
-// void escolherMeioDeTransporteInt(int locomocao) {
-//   if (locomocao == 0) {
-//     print("Carro.");
-//   } else if (locomocao == 1) {
-//     print("Bike.");
-//   } else {
-//     print("Meio de transporte não identificado.");
-//   }
-// }
+  Set<String> registrosVisitados = <String>{};
+  registrosVisitados = registrarDestinos("Rio de Janeiro", registrosVisitados);
+  registrosVisitados = registrarDestinos("São Paulo", registrosVisitados);
+  registrosVisitados = registrarDestinos("Recife", registrosVisitados);
+  registrosVisitados = registrarDestinos("Rio de Janeiro", registrosVisitados);
+
+  print('first:\t${registrosVisitados.first}');
+  print('last:\t${registrosVisitados.last}');
+  print('isEmpty:\t${registrosVisitados.isEmpty}');
+  print('contains("Alagoas"):\t${registrosVisitados.contains("Alagoas")}\n');
+
+  for (final String registro in registrosVisitados) {
+    print(registro);
+  }
+}
 
 enum Transporte { andando, aviao, bike, carro, patins, skate, trem }
 
-void escolherMeioDeTransporteEnum(Transporte locomocao) {
+void escolherMeioDeTransporte(Transporte locomocao) {
   switch (locomocao) {
-    // case Transporte.andando:
-    //   print("Andando.");
-    //   break;
+    case Transporte.andando:
+      print("Andando\n");
+      break;
     case Transporte.aviao:
-      print("Avião.");
+      print("Avião\n");
       break;
     case Transporte.bike:
-      print("Bike.");
+      print("Bike\n");
       break;
     case Transporte.carro:
-      print("Carro.");
+      print("Carro\n");
       break;
     case Transporte.patins:
-      print("Patins.");
+      print("Patins\n");
       break;
     case Transporte.skate:
-      print("Skate.");
+      print("Skate\n");
       break;
     case Transporte.trem:
-      print("Trem.");
+      print("Trem\n");
       break;
     default:
-      print("Meio de transporte não identificado.");
+      print("Meio de transporte não identificado\n");
       break;
   }
+}
+
+Set<String> registrarDestinos(String destino, Set<String> banco) {
+  banco.add(destino);
+  return banco;
 }
